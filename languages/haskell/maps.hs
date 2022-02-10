d6b099f59d48488bd278bffc27eef1d09b8fb1b5
daleksla <salih.msa@outlook.com>
2022-02-10 17:25:51 +0000

    clarified issues with fromJust for lookups and provided case expression based alternative / pattern matching

--- languages/haskell/maps.hs
+++ languages/haskell/maps.hs
@@ -6,11 +6,16 @@
         * Note: maps are defined as a list of pairs (see tuples.hs) of strictly type a and b ( e.g. [(Num, [Char])] )
     * Creating: [ (key, val), (key, val) ]
     * Extracting: lookup <key> <dict> . Note: has function signature of lookup :: Eq a => a -> [(a, b)] -> Maybe b
-        * ... where 'Maybe' is a supertypeclass datatype constructor (datatype constructor = a datatype which needs another type to properly form)
+        * ... where 'Maybe' is a typeclass datatype constructor (datatype constructor = a datatype which needs another type to properly form)
         * ... instances are 'Just' (ie associated when values are found) and 'Nothing' (ie when values are not)
         * ... b is the value associated to a given key
-        * To then use the values (since Maybe pairings prevents values being used naturally), use fromJust function
-            * Requires Data.Maybe module
+        * To then use the values (since Maybe pairings prevents values being used naturally), use either:
+            * case expressions / pattern matching: case result of
+                                                        Just a ...
+                                                        Nothing ...
+            * fromJust: fromJust <Maybe jumble>
+                * NOTE: don't do this because a lack of a Just value results in a crash (ie. if nothing was returned)
+                * Requires Data.Maybe module
     * Note: extended function available in Data.Map module
 -}
 
@@ -24,4 +29,7 @@ lookup "one" myDict -- Just 1
 lookup "zero" myDict -- Nothing
 :info lookup -- lookup :: Eq a => a -> [(a, b)] -> Maybe b . note: Maybe is the 'parent' class of Just and Nothing, b is the datatype of the value
 
-DMaybe.fromJust $ lookup "one" myDict -- 1 . removes weird Maybe appendage
+result = DMaybe.fromJust $ lookup "one" myDict -- 1 . removes weird Maybe appendage BUT this is a weird way to do this as if its nothing it crashes
+case result of -- better
+    Just a -> putStrLn $ "I'm so happy you chose "++show a++"."
+    Nothing -> putStrLn $ "So sorry; "++input++" is not a valid option."
